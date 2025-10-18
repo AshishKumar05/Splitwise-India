32dca89d6cce16c054031e189dbe296f3e7308c7
ashishkumar <ashishgupta.ipec05@gmail.com>
2025-10-18 05:58:07 +0530

    cmp support

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.compose.compiler)        // Kotlin 2.0+ Compose compiler
+    alias(libs.plugins.composeMultiplatform)    // JetBrains Compose MP plugin
 }
 
 kotlin {
@@ -15,7 +17,7 @@ kotlin {
             }
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -28,11 +30,18 @@ kotlin {
     }
 
     sourceSets {
-        commonMain.dependencies {
+        val commonMain by getting {
             //put your multiplatform dependencies here
+            dependencies {
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material3)
+            }
         }
-        commonTest.dependencies {
-            implementation(libs.kotlin.test)
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
         }
     }
 }
